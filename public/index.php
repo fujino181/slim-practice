d95e60b859637a01cb20e101ab0e6e9ffaef9776
Takumi Fujino <tfujino@yahoo-corp.jp>
2017-07-05 00:23:39 +0900

    first commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function($request, $response) {
+  echo 'Hello World!';
+});
+
+$app->get('/json', function($request, $response) {
+  $json = [
+    "hoge" => "fuga",
+    "foo" => "bar",
+  ];
+
+  return $response->withJson($json);
+});
+$app->run();
