63b53d75b7bdffc346cdefb6d66b30ad1df27118
Takumi Fujino <tfujino@yahoo-corp.jp>
2017-07-06 08:44:53 +0900

    [fix] クラス変数の使い方を修正

--- src/DB.php
+++ src/DB.php
@@ -1,25 +1,33 @@
 <?php
-namespace Mynamespace;
+namespace MyNamespace;
 use PDO;
 
 class DB {
-  public $db_ini; 
-  public $pdo; 
+  private $pdo; 
 
   function __construct() {
     $db_ini = parse_ini_file("db.ini");
     print_r($db_ini);
     $database = 'mysql:host=' . $db_ini["hostname"] . ';dbname='. $db_ini["dbname"] .';charset=utf8';
     print_r($database);
-    $pdo = new PDO($database,$db_ini["dbuser"], $db_ini["dbpass"]);
+    try {
+      $this->pdo = new PDO($database,$db_ini["dbuser"], $db_ini["dbpass"]);
+      print_r("get connection");
+    } catch (PDOException $e) {
+      print_r($e->getMessage());
+      throw new PDOException(__FILE__ . __LINE__);
+    } catch (Exception $e) {
+      print_r($e->getMessage());
+      throw new Exception(__FILE__ . __LINE__);
+    }
   }
 
   function select() {
-    $stmt = $pdo->prepare("select * from hoge");
+    $stmt = $this->pdo->prepare("select * from hoge");
     //$stmt->bindValue(1, "value");
     $stmt->execute();
     foreach($stmt as $loop) {
-      echo $loop;
+      print_r($loop);
     }
   }
 }
